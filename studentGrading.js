fbc80c08e352e5a001e727901ef82d384e01675a
GeekyHacks <geekyhacks22@gmail.com>
2023-02-17 11:41:57 +0300

    push

--- studentGrading.js
+++ studentGrading.js
@@ -1,29 +1,17 @@
 function gradingStudents(grades) {
-  // Write your code here
-
   let output = [];
 
   for (let i = 0; i < grades.length; i++) {
-    if (grades[i] >= 38 && grades[i] % 5 === 1) {
+    if (grades[i] > 37) {
       output.push(grades[i]);
-      console.log(output);
-    }
-    if (grades[i] >= 38 && grades[i] % 5 === 2) {
+      if (output[i] % 5 === 4) {
+        output[i] = output[i] + 1;
+      }
+      if (output[i] % 5 === 3) {
+        output[i] = output[i] + 2;
+      }
+    } else {
       output.push(grades[i]);
-      console.log(output);
-    }
-    if (grades[i] >= 38 && grades[i] % 5 === 3) {
-      output.push(grades[i] + 2);
-      console.log(output);
-    }
-    if (grades[i] >= 38 && grades[i] % 5 === 4) {
-      output.push(grades[i] + 1);
-      console.log(output);
-    }
-
-    else  if (grades[i] < 38 && grades[i] > 5) {
-      output.push(grades[i]);
-      console.log(output);
     }
   }
   return output;
